89881a9c57ed69710b4c0a8ca98c3a2df84867a3
TaniguchiNanako <nanakocrimson@gmail.com>
2026-07-16 16:11:26 +0900

    商品モデルの作成 (#10)
    * メニュー機能を追加
    
    * 商品モデルの作成

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+	private int product_id; // 商品ID
+	private String product_name; // 商品名
+	private int product_price; // 値段
+	private int product_stock; // 在庫数
+	private String product_category; // カテゴリー
+	private String product_img_path; // 画像
+	private String product_description; // 商品説明文
+	private LocalDateTime product_created_at; // 登録日時
+	private LocalDateTime product_update_at; // 更新日時
+	private boolean product_active; // 販売状態
+	
+	public Product(int product_id, String product_name, int product_price, int product_stock, String product_category,
+					String product_img_path, String product_description, LocalDateTime product_created_at,
+					LocalDateTime product_update_at, boolean product_active) {
+		this.product_id = product_id;
+		this.product_name = product_name;
+		this.product_price = product_price;
+		this.product_stock = product_stock;
+		this.product_category = product_category;
+		this.product_img_path = product_img_path;
+		this.product_description = product_description;
+		this.product_created_at = product_created_at;
+		this.product_update_at = product_update_at;
+		this.product_active = product_active;
+	}
+	
+	
+
+}
--- src/main/webapp/WEB-INF/jsp/menu.jsp
+++ src/main/webapp/WEB-INF/jsp/menu.jsp
@@ -4,13 +4,13 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Iメニュー画面</title>
+<title>メニュー画面</title>
 </head>
 <body>
 <h1>メニュー</h1>
 <ul>
 	<li><a href="ProductListServlet">商品を選択する</a></li>
-	<li><a href="CartViewServlet">ショッピングカートを見る</a><li>
+	<li><a href="CartServlet">ショッピングカートを見る</a><li>
 	<li><a href="OrderHistoryServlet">注文履歴を見る</a></li>
 	<li><a href="MemberEditServlet">会員情報の変更</a></li>
 </ul>
