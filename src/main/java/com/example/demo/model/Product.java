e4c536b53b803bb74bf34e5ac9bfd2e5e74ca57d
TaniguchiNanako <nanakocrimson@gmail.com>
2026-07-17 16:54:12 +0900

    商品情報DAO作成

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -7,21 +7,23 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 //キャメルケースでお願いします（修正してください）
 //AllArgsを使用していますので、全項目のコンストラクタは必要ないです
+@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product implements Serializable {
-	private int productAd; // 商品ID
+	private int productId; // 商品ID
 	private String productName; // 商品名
 	private int productPrice; // 値段
 	private int productStock; // 在庫数
 	private String productCategory; // カテゴリー
 	private String productImgPath; // 画像
 	private String productDescription; // 商品説明文
-	private LocalDateTime productCreated_at; // 登録日時
+	private LocalDateTime productCreatedAt; // 登録日時
 	private LocalDateTime productUpdateAt; // 更新日時
 	private boolean productActive; // 販売状態	
 
