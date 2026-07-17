4b808c2ca93c8558a46e3eaae121115250001c91
RYO YANASE <yanase0925munakata@gmail.com>
2026-07-17 21:10:09 +0900

    ログインの処理追加とそれに伴う修正

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,31 +1,49 @@
 package com.example.demo.controller;
+
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.dao.AccountDAO;
 import com.example.demo.model.Account;
 
+//処理の不足を修正
+
 //ログイン
 @Controller
 public class LoginController {
+	
+	
+	@GetMapping("/login")
+	public String showLogin() {
+		return "index";
+	}
+	
 	@PostMapping("/login")
 	public String login( 
-			@RequestParam String accountName,
+			@RequestParam String accountId,
             @RequestParam String accountPass,
             HttpSession session
 			) {
 	AccountDAO dao = new AccountDAO();
+	Account account = dao.login(accountId, accountPass);
 	
-	Account account = dao.login(accountName, accountPass);
-	
-	if(account != null) {
-		session.setAttribute("account", account); 
-		return"menu";
-	}else {
-		return"index";
+//	アカウントがない場合indexに返す
+	if(account == null) {
+		return "index";
 	}
+	
+	session.setAttribute("account", account); 
+	
+//	accountsのadminがtrueならadminのmenuに
+//	Lombok使ってるのでisAdmin
+	if(account.isAdmin() == true) {
+		return"admin/admin-menu";
+	}
+	
+	return "menu";
 	}
 }
