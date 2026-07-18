b9a7c4ab910a1f0520b030bbe912b8cc4feffce1
柳瀨　諒 <yanase0925munakata@gmail.com>
2026-07-18 10:14:36 +0900

    名前とパスでLogin→IDとパスでLoginに修正・BCryptの準備・Webfilterの作成、aaccount基本処理の作成 (#21)
    * ログインの処理追加とそれに伴う修正
    
    * Utilと認証Filterを追加
    
    * 会員登録・更新処理

--- src/main/java/com/example/demo/dao/AccountDAO.java
+++ src/main/java/com/example/demo/dao/AccountDAO.java
@@ -4,8 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.example.demo.model.Account;
+import com.example.demo.util.DBUtil;
+
+//accountNameとaccountPassでログインすることになってたので修正しました（柳瀬）
 
 //accountIdとaccountNameで読むようになってるので修正しました（柳瀬）
 
@@ -74,5 +78,123 @@ public class AccountDAO {
     }
 
     return result;
+    }
+    
+// // accountsTABLEにaccount_activeを追加し論理削除する退会処理の例
+//    public boolean deleteAccount(String accountId) {
+//    	
+//    	String sql = """
+//    			UPDATE accounts
+//    			SET account_active = false
+//    			WHERE account_id = ?
+//    			  AND account_active = true
+//    			""";
+//    	
+//    	try (
+//    			Connection conn = DBUtil.getConnection();
+//    			PreparedStatement pStmt = conn.prepareStatement(sql)
+//    	) {
+//    		pStmt.setString(1, accountId);
+//    		int count = pStmt.executeUpdate();
+//    		
+//    		return count == 1;
+//    		
+//    	} catch (SQLException e) {
+//    		e.printStackTrace();
+//    		return false;
+//    		}
+//    }
+    
+//    会員登録
+    public boolean insert(Account account) {
+	    	String sql = """
+	    			INSERT INTO accounts(
+	    			account_id,
+	    			account_name,
+	    			account_pass,
+	    			postal_code,
+	    			account_address,
+	    			account_phone,
+	    			birthday,
+	    			email,
+	    			payment,
+	    			admin
+	    			)
+	    			VALUES(?,?,?,?,?,?,?,?,?,?)
+	    			""";
+	    	
+	    	try (Connection conn = DBUtil.getConnection();
+	    	PreparedStatement pStmt = conn.prepareStatement(sql)
+	    	){
+	    		pStmt.setString(1, account.getAccountId());
+	    		pStmt.setString(2, account.getAccountName());
+	    		pStmt.setString(3, account.getAccountPass());
+	    		pStmt.setString(4, account.getPostalCode());
+	    		pStmt.setString(5, account.getAccountAddress());
+	    		pStmt.setString(6, account.getAccountPhone());
+	    		if (account.getBirthday() != null) {
+	    			pStmt.setDate(
+	    					7,
+	    					java.sql.Date.valueOf(account.getBirthday()));
+	    		} else {
+	    			pStmt.setDate(7, null);
+	    		}
+	    		pStmt.setString(8, account.getEmail());
+	    		pStmt.setString(9, account.getPayment());
+	    		pStmt.setBoolean(10,account.isAdmin());
+	    		
+	    		int count = pStmt.executeUpdate();
+	    		
+	    		return count == 1;
+	    	}catch(SQLException e) {
+	    		e.printStackTrace();
+	    		return false;
+	    		
+	    	}
+	    		
+    }
+    
+//    会員情報更新
+    public boolean update(Account account) {
+    		String sql = """
+    				UPDATE accounts
+    				SET
+    				account_name = ?,
+    				postal_code = ?,
+    				account_address = ?,
+    				account_phone = ?,
+    				birthday = ?,
+    				email = ?,
+    				payment = ?
+    				WHERE account_id = ?
+    				""";
+    		
+    		try (Connection conn = DBUtil.getConnection();
+    		    	PreparedStatement pStmt = conn.prepareStatement(sql)
+    		    	){
+    			pStmt.setString(1, account.getAccountName());
+    			pStmt.setString(2, account.getPostalCode());
+    			pStmt.setString(3, account.getAccountAddress());
+    			pStmt.setString(4, account.getAccountPhone());
+    			if (account.getBirthday() != null) {
+    				pStmt.setDate(
+    						5,
+    						java.sql.Date.valueOf(account.getBirthday()));
+    			} else {
+    				pStmt.setDate(5, null);
+    			}
+    			pStmt.setString(6, account.getEmail());
+    			pStmt.setString(7, account.getPayment());
+    			pStmt.setString(8, account.getAccountId());
+    			
+    			int count = pStmt.executeUpdate();
+    			
+    			return count == 1;
+    			
+    		} catch (SQLException e) {
+    			e.printStackTrace();
+    			return false;
+    		}
+    	}
 }
-}
+
