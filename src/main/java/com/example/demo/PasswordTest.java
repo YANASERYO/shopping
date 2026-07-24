5e637c2fd2986f35b66e5fbf6be01ad79f6fcb93
柳瀨　諒 <yanase0925munakata@gmail.com>
2026-07-24 12:13:11 +0900

    Feature/product management (#28)
    * 暫定的な環境整備と体力的に・・・
    
    * 注文確定処理とカート機能を追加
    
    * 起動できるように修正

---
+++ src/main/java/com/example/demo/PasswordTest.java
@@ -0,0 +1,13 @@
+//package com.example.demo;
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//public class PasswordTest {
+//
+//    public static void main(String[] args) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//        System.out.println("adminpass: " + encoder.encode("adminpass"));
+//        System.out.println("userpass: " + encoder.encode("userpass"));
+//    }
+//}
